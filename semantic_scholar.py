5de6df5470561c1dcaf24ce5fa42af001fd6be82
Olaf Apelseth Liadal <olaf.liadal@gmail.com>
2021-04-24 20:03:52 +0200

    Replace requests-cache with aio-http-client-cache
    Also use aiolimiter to throttle request rate.

--- semantic_scholar.py
+++ semantic_scholar.py
@@ -1,8 +1,7 @@
-from requests_cache import CachedSession
-from datetime import datetime, timedelta
-from itertools import dropwhile
-from time import sleep
-from typing import List
+from aiohttp_client_cache import CachedSession, SQLiteBackend
+from aiolimiter import AsyncLimiter
+from datetime import timedelta
+from typing import Awaitable
 
 
 class SemanticScholar:
@@ -14,34 +13,29 @@ class SemanticScholar:
         :param max_requests: Max number of requests per window.
         :param window_size: Window size in seconds.
         """
-        self.max_requests = max_requests
-        self.window_size = timedelta(seconds=window_size)
-        self.window_requests: List[datetime] = []
-        self.session = CachedSession(expire_after=300)
-        if api_key is not None:
-            self.session.headers.update({"x-api-key": api_key})
-            self.base_url = "https://partner.semanticscholar.org/v1"
-        else:
-            self.base_url = "https://api.semanticscholar.org/v1"
-
-    def _get(self, endpoint: str, **kwargs):
-        now = datetime.now()
-        self.window_requests = list(
-            dropwhile(lambda t: t < now - self.window_size, self.window_requests)
+        self._limiter = AsyncLimiter(max_requests, window_size)
+        self._session = CachedSession(
+            cache=SQLiteBackend(expire_after=timedelta(minutes=150))
         )
-        if len(self.window_requests) >= self.max_requests:
-            sleep_duration = self.window_size - (now - self.window_requests[0])
-            sleep(sleep_duration.total_seconds())
-        self.window_requests.append(now)
+        if api_key is not None:
+            self._session.headers.update({"x-api-key": api_key})
+            self._base_url = "https://partner.semanticscholar.org/v1"
+        else:
+            self._base_url = "https://api.semanticscholar.org/v1"
 
-        return self.session.get(f"{self.base_url}{endpoint}", **kwargs).json()
+    async def _get(self, endpoint: str, **kwargs):
+        async with self._limiter:
+            res = await self._session.get(f"{self._base_url}{endpoint}", **kwargs)
+            return await res.json()
 
-    def get_paper(self, s2_id: str = None, arxiv_id: str = None, **kwargs) -> dict:
+    async def get_paper(
+        self, s2_id: str = None, arxiv_id: str = None, **kwargs
+    ) -> Awaitable[dict]:
         if sum(i is None for i in (s2_id, arxiv_id)) != 1:
             raise ValueError("Exactly one type of paper ID must be provided.")
 
         paper_id = s2_id if s2_id is not None else f"arXiv:{arxiv_id}"
-        return self._get(f"/paper/{paper_id}", **kwargs)
+        return await self._get(f"/paper/{paper_id}", **kwargs)
 
-    def get_author(self, s2_id: str, **kwargs) -> dict:
-        return self._get(f"/author/{s2_id}", **kwargs)
+    async def get_author(self, s2_id: str, **kwargs) -> Awaitable[dict]:
+        return await self._get(f"/author/{s2_id}", **kwargs)
