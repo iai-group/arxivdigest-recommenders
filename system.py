e81a38257235127863e7d6bf433545ecb79801e3
Olaf Apelseth Liadal <olaf.liadal@gmail.com>
2021-04-29 20:07:32 +0200

    Increase concurrency

--- system.py
+++ system.py
@@ -1,7 +1,8 @@
 import asyncio
 import random
 from datetime import date
-from typing import List, Dict
+from collections import defaultdict
+from typing import List, Dict, Any
 from arxivdigest.connector import ArxivdigestConnector
 
 from semantic_scholar import SemanticScholar
@@ -21,9 +22,18 @@ class RecommenderSystem:
         """
         self._connector = arxivdigest_connector
         self._max_paper_age = max_paper_age
-        self._article_ids = self._connector.get_article_ids()
+
+        # Stores the names of all discovered venues.
         self._venues: List[str] = []
+
+        # Stores the vector representations of the papers (their authors) that are candidate for recommendation.
+        self._papers: Dict[str, List[Dict[str, Any]]] = {}
+
+        # Used as a cache for the vector representations of authors.
         self._authors: Dict[str, List[int]] = {}
+
+        # Locks used to ensure that the vector representation of an author is created only once if missing.
+        self._author_locks = defaultdict(asyncio.Lock)
 
     async def get_author_representation(self, s2_id: str) -> List[int]:
         """Get the vector representation of an author.
@@ -35,36 +45,72 @@ class RecommenderSystem:
         :param s2_id: S2 author ID.
         :return: Author vector representation.
         """
-        if s2_id not in self._authors:
-            year_cutoff = date.today().year - self._max_paper_age
-            async with SemanticScholar() as s2:
-                author = await s2.get_author(s2_id)
-                papers = await asyncio.gather(
-                    *[
-                        s2.get_paper(s2_id=paper["paperId"])
-                        for paper in author["papers"]
-                        if paper["year"] is None or paper["year"] >= year_cutoff
-                    ],
-                    return_exceptions=True,
-                )
-            papers = [paper for paper in papers if not isinstance(paper, Exception)]
-            author_venues = [paper["venue"] for paper in papers if paper["venue"]]
+        async with self._author_locks[s2_id]:
+            if s2_id not in self._authors:
+                year_cutoff = date.today().year - self._max_paper_age
+                async with SemanticScholar() as s2:
+                    author = await s2.get_author(s2_id)
+                    papers = await asyncio.gather(
+                        *[
+                            s2.get_paper(s2_id=paper["paperId"])
+                            for paper in author["papers"]
+                            if paper["year"] is None or paper["year"] >= year_cutoff
+                        ],
+                        return_exceptions=True,
+                    )
+                papers = [paper for paper in papers if not isinstance(paper, Exception)]
+                author_venues = [paper["venue"] for paper in papers if paper["venue"]]
 
-            for venue in author_venues:
-                if (
-                    venue.lower() not in config.VENUE_BLACKLIST
-                    and venue not in self._venues
-                ):
-                    self._venues.append(venue)
+                for venue in author_venues:
+                    if (
+                        venue.lower() not in config.VENUE_BLACKLIST
+                        and venue not in self._venues
+                    ):
+                        self._venues.append(venue)
 
-            self._authors[s2_id] = [
-                author_venues.count(venue) for venue in self._venues
-            ]
+                self._authors[s2_id] = [
+                    author_venues.count(venue) for venue in self._venues
+                ]
         return self._authors[s2_id]
+
+    async def gen_paper_representations(self):
+        """Generate author representations for each author of each paper that is candidate for recommendation."""
+
+        async def get_author_representations(paper: dict):
+            authors = await asyncio.gather(
+                *[
+                    self.get_author_representation(author["authorId"])
+                    for author in paper["authors"]
+                    if author["authorId"]
+                ],
+                return_exceptions=True,
+            )
+            return [
+                {"name": paper["authors"][i]["name"], "representation": author}
+                for i, author in enumerate(authors)
+                if not isinstance(author, Exception)
+            ]
+
+        paper_ids = self._connector.get_article_ids()
+        logger.info(
+            f"Generating vector representations of {len(paper_ids)} candidate papers and their authors."
+        )
+        async with SemanticScholar() as s2:
+            papers = await asyncio.gather(
+                *[s2.get_paper(arxiv_id=paper_id) for paper_id in paper_ids],
+                return_exceptions=True,
+            )
+        papers = [paper for paper in papers if not isinstance(paper, Exception)]
+        paper_authors = await asyncio.gather(
+            *[get_author_representations(paper) for paper in papers]
+        )
+        self._papers = {
+            paper["arxivId"]: authors for paper, authors in zip(papers, paper_authors)
+        }
 
     def gen_explanation(
         self, user: List[int], author: List[int], author_name: str, max_venues=3
-    ):
+    ) -> str:
         """Generate a recommendation explanation based on the vector representations of a user and an author.
 
         :param user: User vector representation.
@@ -107,56 +153,35 @@ class RecommenderSystem:
                 f"{'this venue' if len(frequent_venues) == 1 else 'these venues'} in the same time period."
             )
 
-    async def gen_user_ranking(self, s2_id: str) -> List[dict]:
-        """Generate article ranking for a single user.
+    async def gen_user_ranking(self, s2_id: str) -> List[Dict[str, Any]]:
+        """Generate paper ranking for a single user.
 
-        The score assigned to each article is the cosine similarity between the user and the article author that is
+        The score assigned to each paper is the cosine similarity between the user and the paper author that is
         most similar to the user.
 
         :param s2_id: S2 author ID of the user.
-        :return: Ranking of candidate articles.
+        :return: Ranking of candidate papers.
         """
         try:
             user = await self.get_author_representation(s2_id)
         except Exception:
             logger.error(f"S2 ID {s2_id}: unable to generate author representation.")
             return []
-        async with SemanticScholar() as s2:
-            articles = await asyncio.gather(
-                *[
-                    s2.get_paper(arxiv_id=article_id)
-                    for article_id in self._article_ids
-                ],
-                return_exceptions=True,
-            )
-        articles = [
-            article for article in articles if not isinstance(article, Exception)
-        ]
         results = []
-        for article in articles:
-            authors = await asyncio.gather(
-                *[
-                    self.get_author_representation(author["authorId"])
-                    for author in article["authors"]
-                    if author["authorId"]
+        for paper_id, authors in self._papers.items():
+            similar_author, score = max(
+                [
+                    (author, padded_cosine_sim(user, author["representation"]))
+                    for author in authors
                 ],
-                return_exceptions=True,
-            )
-            authors = [
-                (author, article["authors"][i]["name"])
-                for i, author in enumerate(authors)
-                if not isinstance(author, Exception)
-            ]
-            (similar_author, similar_author_name), score = max(
-                [(author, padded_cosine_sim(user, author[0])) for author in authors],
                 key=lambda t: t[1],
             )
             results.append(
                 {
-                    "article_id": article["arxivId"],
+                    "article_id": paper_id,
                     "score": score,
                     "explanation": self.gen_explanation(
-                        user, similar_author, similar_author_name
+                        user, similar_author["representation"], similar_author["name"]
                     )
                     if score > 0
                     else "",
@@ -165,44 +190,54 @@ class RecommenderSystem:
         return results
 
     async def gen_recommendations(
-        self, users: dict, interleaved_articles: dict, max_recommendations=10
-    ) -> dict:
+        self, users: dict, interleaved_papers: dict, max_recommendations=10
+    ) -> Dict[str, Dict[str, Any]]:
         """Generate recommendations for a user batch.
 
         :param users: Users.
-        :param interleaved_articles: Interleaved articles that will be excluded from the generated recommendations
+        :param interleaved_papers: Interleaved papers that will be excluded from the generated recommendations
         before submission.
         :param max_recommendations: Max number of recommendations per user.
         :return: Recommendations.
         """
-        recommendations = {}
-        for user_id, user in users.items():
-            s2_id = extract_s2_id(user)
+
+        async def gen_user_recommendations(user_id: str, user_data: dict):
+            s2_id = extract_s2_id(user_data)
             if s2_id is None:
                 logger.info(f"User {user_id}: skipped (no S2 ID provided).")
-                continue
-            logger.debug(f"User {user_id} S2 ID: {s2_id}.")
+                return []
             user_ranking = await self.gen_user_ranking(s2_id)
             user_ranking = [
                 recommendation
                 for recommendation in user_ranking
-                if recommendation["article_id"] not in interleaved_articles[user_id]
+                if recommendation["article_id"] not in interleaved_papers[user_id]
                 and recommendation["score"] > 0
             ]
             user_recommendations = sorted(
                 user_ranking, key=lambda r: r["score"], reverse=True
-            )
-            if len(user_recommendations) > 0:
-                recommendations[user_id] = user_recommendations[:max_recommendations]
+            )[:max_recommendations]
             logger.info(
-                f"User {user_id}: recommended {len(recommendations[user_id])} articles."
+                f"User {user_id}: recommended {len(user_recommendations)} papers."
             )
-        return recommendations
+            return user_recommendations
+
+        recommendations = await asyncio.gather(
+            *[
+                gen_user_recommendations(user_id, user_data)
+                for user_id, user_data in users.items()
+            ]
+        )
+        return {
+            user_id: user_recommendations
+            for user_id, user_recommendations in zip(users.keys(), recommendations)
+            if len(user_recommendations) > 0
+        }
 
     async def recommend(self):
         """Generate and submit recommendations for all users."""
         total_users = self._connector.get_number_of_users()
-        logger.info(f"Starting recommending articles for {total_users} users")
+        logger.info(f"Recommending papers for {total_users} users.")
+        await self.gen_paper_representations()
         recommendation_count = 0
         while recommendation_count < total_users:
             user_ids = self._connector.get_user_ids(recommendation_count)
@@ -214,7 +249,8 @@ class RecommenderSystem:
             if recommendations:
                 self._connector.send_article_recommendations(recommendations)
             recommendation_count += len(user_ids)
-            logger.info(f"Processed {recommendation_count} users")
+            logger.info(f"Processed {recommendation_count} users.")
+        logger.info("Finished recommending.")
 
 
 if __name__ == "__main__":
@@ -223,4 +259,3 @@ if __name__ == "__main__":
     )
     recommender = RecommenderSystem(arxivdigest_connector)
     asyncio.run(recommender.recommend())
-    logger.info("\nFinished recommending articles.")
